9bc29db1ccc0744097ce940dcc43c6171659022a
Kevin Lucido <kevinjoshualucido@gmail.com>
2023-10-08 19:50:48 -0500

    Update

---
+++ 2_Intro_to_Programming/Lucido_Kevin_JS_CreateVariaibles.js
@@ -0,0 +1,7 @@
+// Variables for local theme park requirements in order to be eligible to ride rollercoaster.
+
+// Rider must be at least 42 inches tall in order to ride (min_height >= 42).
+var min_height = 42;
+
+// Rider must be over the age of 10 (min_age > 10).
+var min_age = 11;
