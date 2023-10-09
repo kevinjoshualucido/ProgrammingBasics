729cc8d00f92718f3e0ac6f25e6ee8fef64bbee2
Kevin Lucido <kevinjoshualucido@gmail.com>
2023-10-08 20:51:17 -0500

    Update

---
+++ 2_Intro_to_Programming/prac/code.js
@@ -0,0 +1,8 @@
+// Assigns a number value to the variable
+var likeCount = 0;
+
+// It creates a like count function
+function increaseLikes() {
+    // increases the likeCount by 1
+    likeCount = likeCount + 1;
+}
