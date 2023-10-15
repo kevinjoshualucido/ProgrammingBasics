4bf3eee3abdd4a61c1fa526f4e7212182669ea54
Kevin Lucido <kevinjoshualucido@gmail.com>
2023-10-15 12:22:58 -0500

    End of Week 1

--- 2_Intro_to_Programming/Lucido_Kevin_JS_CreateVariaibles.js
+++ 2_Intro_to_Programming/Lucido_Kevin_JS_CreateVariaibles.js
@@ -2,6 +2,27 @@
 
 // Rider must be at least 42 inches tall in order to ride (min_height >= 42).
 var min_height = 42;
+// Rider must be over the age of 10 (min_age >= 10).
+var min_age = 10;
 
-// Rider must be over the age of 10 (min_age > 10).
-var min_age = 11;
+
+if (min_height >= 42) {
+    console.log("Get on that ride, kiddo!");
+} else {
+    console.log("Sorry kiddo. Maybe next year");
+}
+
+
+// Stretch Feature 1
+if (min_height >= 42 || min_age >= 10) {
+    console.log("Get on that ride, kiddo!");
+} else {
+    console.log("Sorry kiddo. Maybe next year");
+}
+
+// Stretch Feature 2
+if (min_height >= 42 && min_age >= 10) {
+    console.log("Get on that ride, kiddo!");
+} else {
+    console.log("Sorry kiddo. Maybe next year");
+}
